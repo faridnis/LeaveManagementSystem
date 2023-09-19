b69879fbc275f720ffc9ff7c4e002be9ab30c6e1
faridnice <faridanuar26@gmail.com>
2023-09-19 11:31:14 +0800

    Add categoriesform and link with database

--- CategoriesForm.cs
+++ CategoriesForm.cs
@@ -12,9 +12,48 @@ namespace LeaveManagementSystem
 {
     public partial class CategoriesForm : Form
     {
+        Functions Con;
         public CategoriesForm()
         {
+            
             InitializeComponent();
+            Con = new Functions();
+            ShowCategories();
+        }
+        private void ShowCategories()
+        {
+            string Query = "SELECT * FROM CategoryTb";
+            CategoriesList.DataSource = Con.GetData(Query);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnSaveCategories_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtCatName.Text == "")
+                {
+                    MessageBox.Show("Missing Data!");
+                }
+                else
+                {
+                    string Category = txtCatName.Text;
+                    string Query = "INSERT INTO CategoryTb VALUES('{0}')";
+                    Query = string.Format(Query, Category);
+                    Con.SetData(Query);
+                    ShowCategories();
+                    txtCatName.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
